e03a97a2e5a85e06a20e948c7de2698a68cba632
T. Hamilton <profhamilton3@gmail.com>
2024-11-07 15:00:00 -0800

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
-sprite = game.create_sprite(2, 2)
+def isHit(sprite: game.LedSprite, chase: game.LedSprite):
+    return False 
+
+def on_button_pressed_a():
+    catcher.move(-1)
+    if True:
+        pass
+    else:
+        pass
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    catcher.change(LedSpriteProperty.Y, -1)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    catcher.move(1)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_logo_pressed():
+    catcher.change(LedSpriteProperty.Y, 1)
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
+
+catcher: game.LedSprite = None
+sprite2 = game.create_sprite(2, 2)
 catcher = game.create_sprite(4, 4)
+randomangle = 45
+MaxTime = 5000
+
 def on_forever():
-    sprite.move(1)
-    sprite.turn(Direction.RIGHT, 45)
-    sprite.if_on_edge_bounce()
+    global randomangle
+    sprite2.move(1)
+    basic.pause(25)
+    sprite2.turn(Direction.RIGHT, randomangle)
+    basic.pause(100)
+    sprite2.if_on_edge_bounce()
+    randomangle = randint(30, 320)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+function isHit (sprite: game.LedSprite, chase: game.LedSprite) {
+    if (sprite.isTouching(chase)) {
+        music.play(music.tonePlayable(988, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+        return true
+    }
+    return false
+}
 input.onButtonPressed(Button.A, function () {
     catcher.move(-1)
 })
@@ -11,12 +18,16 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     catcher.change(LedSpriteProperty.Y, 1)
 })
 let catcher: game.LedSprite = null
-let sprite = game.createSprite(2, 2)
+let sprite2 = game.createSprite(2, 2)
 catcher = game.createSprite(4, 4)
+let randomangle = 45
+let MaxTime = 5000
 basic.forever(function () {
-    sprite.move(1)
+    sprite2.move(1)
     basic.pause(25)
-    sprite.turn(Direction.Right, 45)
+    sprite2.turn(Direction.Right, randomangle)
     basic.pause(100)
-    sprite.ifOnEdgeBounce()
+    sprite2.ifOnEdgeBounce()
+    randomangle = randint(30, 320)
+    isHit(sprite2, catcher)
 })
