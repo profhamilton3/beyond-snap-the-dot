e1e50df89686c0d7247cd80b2c9c6ec7e510c317
T. Hamilton <profhamilton3@gmail.com>
2024-11-08 19:06:16 -0800

    moved hit test to background - works smoother

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.down, function () {
+    music.playTone(165, music.beat(BeatFraction.Half))
     catcher.change(LedSpriteProperty.Y, 1)
 })
 function isHit (sprite: game.LedSprite, chase: game.LedSprite) {
     if (sprite.isTouching(chase)) {
-        music.play(music.tonePlayable(988, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(988, music.beat(BeatFraction.Quarter)), music.PlaybackMode.InBackground)
+        joystickbit.Vibration_Motor(100)
         return true
     }
     return false
@@ -13,6 +15,7 @@ input.onButtonPressed(Button.A, function () {
 })
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.down, function () {
     catcher.move(1)
+    music.playTone(175, music.beat(BeatFraction.Half))
 })
 input.onButtonPressed(Button.AB, function () {
     catcher.change(LedSpriteProperty.Y, 1)
@@ -21,10 +24,12 @@ input.onButtonPressed(Button.B, function () {
     catcher.move(1)
 })
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function () {
+    music.playTone(156, music.beat(BeatFraction.Half))
     catcher.change(LedSpriteProperty.Y, -1)
 })
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.down, function () {
     catcher.move(-1)
+    music.playTone(131, music.beat(BeatFraction.Half))
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     catcher.change(LedSpriteProperty.Y, -1)
@@ -35,31 +40,31 @@ catcher = game.createSprite(4, 4)
 let randomangle = 45
 joystickbit.initJoystickBit()
 basic.forever(function () {
-    sprite2.move(1)
-    basic.pause(25)
-    sprite2.turn(Direction.Right, randomangle)
-    basic.pause(100)
-    sprite2.ifOnEdgeBounce()
-    randomangle = randint(30, 320)
-    isHit(sprite2, catcher)
+    if (joystickbit.getRockerValue(joystickbit.rockerType.X) < 200) {
+        catcher.move(1)
+        music.playTone(262, music.beat(BeatFraction.Half))
+    }
+    if (joystickbit.getRockerValue(joystickbit.rockerType.X) >= 800) {
+        catcher.move(-1)
+        music.playTone(294, music.beat(BeatFraction.Half))
+    }
+    if (joystickbit.getRockerValue(joystickbit.rockerType.Y) >= 800) {
+        catcher.change(LedSpriteProperty.Y, -1)
+        music.playTone(494, music.beat(BeatFraction.Half))
+    }
+    if (joystickbit.getRockerValue(joystickbit.rockerType.Y) <= 200) {
+        catcher.change(LedSpriteProperty.Y, 1)
+        music.playTone(440, music.beat(BeatFraction.Half))
+    }
 })
 control.inBackground(function () {
     while (true) {
-        if (joystickbit.getRockerValue(joystickbit.rockerType.X) < 200) {
-            catcher.move(1)
-            music.playTone(262, music.beat(BeatFraction.Half))
-        }
-        if (joystickbit.getRockerValue(joystickbit.rockerType.X) >= 800) {
-            catcher.move(-1)
-            music.playTone(294, music.beat(BeatFraction.Half))
-        }
-        if (joystickbit.getRockerValue(joystickbit.rockerType.Y) >= 800) {
-            catcher.change(LedSpriteProperty.Y, -1)
-            music.playTone(494, music.beat(BeatFraction.Half))
-        }
-        if (joystickbit.getRockerValue(joystickbit.rockerType.Y) <= 200) {
-            catcher.change(LedSpriteProperty.Y, 1)
-            music.playTone(440, music.beat(BeatFraction.Half))
-        }
+        sprite2.move(1)
+        basic.pause(25)
+        sprite2.turn(Direction.Right, randomangle)
+        basic.pause(100)
+        sprite2.ifOnEdgeBounce()
+        randomangle = randint(30, 320)
+        isHit(sprite2, catcher)
     }
 })
